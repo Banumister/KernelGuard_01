f06bd0ec8ab4ade99acf2bc80099cf99f8e632ad
Banumister <bhanumiller@gmail.com>
2026-08-11 11:49:31 +0530

    feat(ebpf): add data structures for tcp_connect tracking

---
+++ ebpf/execve_trace.c
@@ -0,0 +1,11 @@
+/* --- tcp_connect() tracking (Week 2) --- */
+
+struct connect_data_t {
+    u32 pid;
+    u32 saddr;
+    u32 daddr;
+    u16 dport;
+};
+
+BPF_HASH(currsock, u32, struct sock *);
+BPF_PERF_OUTPUT(tcp_events);
