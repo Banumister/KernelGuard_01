77b4ee50b4fb7bd783fb11e7231db85cfebe1f6b
Banumister <bhanumiller@gmail.com>
2026-08-11 11:53:17 +0530

    feat(ebpf): add tcp_connect entry probe to track in-flight sockets

--- ebpf/execve_trace.c
+++ ebpf/execve_trace.c
@@ -9,3 +9,10 @@ struct connect_data_t {
 
 BPF_HASH(currsock, u32, struct sock *);
 BPF_PERF_OUTPUT(tcp_events);
+
+int trace_connect_entry(struct pt_regs *ctx, struct sock *sk)
+{
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    currsock.update(&pid, &sk);
+    return 0;
+}
